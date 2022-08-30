98041683c90cd88607f814bd5f7d6f879eaa6af6
Ubervisor [Dan0xE] <danopperm@gmail.com>
2022-08-30 02:10:52 +0200

    More doc

--- example/src/utils/queryLocal.ts
+++ example/src/utils/queryLocal.ts
@@ -1,3 +1,4 @@
+/* Query for an object and a specified prop */
 export async function queryByProp(query: string, prop: string): Promise<any> {
     let items = JSON.parse(localStorage.getItem(query)!);
     try {
@@ -7,6 +8,7 @@ export async function queryByProp(query: string, prop: string): Promise<any> {
     }
 }
 
+/* Query an object, a specified prop and its assigned value and get it returned */
 export async function queryByPropAndValue(query: string, prop: string, value: string): Promise<any> {
     let items = JSON.parse(localStorage.getItem(query)!);
     try {
@@ -16,6 +18,7 @@ export async function queryByPropAndValue(query: string, prop: string, value: st
     }
 }
 
+/* Query an Object from LocalStorage */
 export async function queryItem(query: string): Promise<any> {
     let item = JSON.parse(localStorage.getItem(query)!);
     if (item === null) {
